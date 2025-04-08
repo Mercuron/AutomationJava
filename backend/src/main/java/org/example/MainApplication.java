41d2733e799842b30464cdf8136475c5d10d65a5
Sokolov.VS <sokolov.vs@geobuilder.ru>
2025-04-08 18:04:47 +0300

    add comment

--- backend/src/main/java/org/example/MainApplication.java
+++ backend/src/main/java/org/example/MainApplication.java
@@ -3,6 +3,7 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//test
 @SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
